9f5a7b6584c05a6dca0a725c24fcd3ccbaf4e276
ARIZYS <stasprogramm111@gmail.com>
2023-10-19 18:50:38 +0300

    commit 0

---
+++ bot telegram/works bot.py
@@ -0,0 +1,66 @@
+import telebot
+from telebot import types
+import webbrowser
+
+bot = telebot.TeleBot("6471873793:AAGwJAy5u8Puj8HxIAOyBJegV9P9QaIhrmQ")
+
+@bot.message_handler(commands= ["start"])
+def start(message):
+    markup = types.ReplyKeyboardMarkup()
+    b1 = types.KeyboardButton("ARIZYS")
+    markup.row(b1)
+    b2 = types.KeyboardButton("delete photo")
+    markup.row(b2)
+    b3 = types.KeyboardButton("edit")
+    markup.row(b3)
+    bot.send_message(message.chat.id,"HELLO", reply_markup=markup)
+
+@bot.message_handler(commands=["site"])
+def site(message):
+    webbrowser.open("https://www.youtube.com/@ARIZYS")
+
+
+@bot.message_handler(commands=["start"])
+def get_p(message):
+    markup = types.InlineKeyboardMarkup()
+    bs = types.InlineKeyboardButton("RKSI", url="https://www.rksi.ru/")
+
+@bot.message_handler(commands=["help"])
+def main(message):
+        bot.send_message(message.chat.id, "I can help you")
+
+@bot.message_handler(commands=["Happy"])
+def site(message):
+    webbrowser.open("https://www.youtube.com/watch?v=3EfHQzRKLL0")
+
+@bot.message_handler(commands=["true"])
+def main(message):
+    bot.send_message(message.chat.id, "цм")
+
+
+
+
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_message(callback):
+    if callback.data == "delete":
+        bot.delete_message(callback.message.chat.id, callback.message.message_id)
+    elif callback == "edit":
+      bot.edit_message_text("edit text", callback.message.chat.id, callback.message.message_id)
+
+
+
+@bot.message_handler(content_types=["message"])
+def get_p(message):
+    markup = types.InlineKeyboardMarkup()
+    buttimes1 = types.InlineKeyboardButton("RKSI", Url= "https://www.rksi.ru/")
+
+
+
+
+
+
+
+bot.polling(none_stop=True)
+
+
+
---
+++ gamelton/__init__.py
@@ -0,0 +1,7 @@
+a = 50
+b = 100
+c = 40
+max = a if a > b else b
+max = c if c > max else max
+print(max)
+
